7129ec307b282fdd9dbce6d79d5935613fdbbb6e
harshitarora13 <its.harshit.arora@gmail.com>
2023-10-06 11:55:18 +1100

    finished project

--- server.js
+++ server.js
@@ -1,27 +1,63 @@
 let express = require('express');
 let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://iamadmin:adminiam@cluster0.ga1jyuh.mongodb.net/?retryWrites=true&w=majority";
 let port = process.env.port || 3000;
+let collection;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req,res)=>{
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  
+  async function runDBConnection() {
+    try {
+      await client.connect();
+      collection = client.db().collection('Cat');
+      console.log(collection);
+    } catch(ex) {
+      console.error(ex);
+    }
+  }
+
+app.get('/', function (req,res) {
     res.render('index.html');
 });
 
-app.get('/addTwoNumbers',(req,res)=>{
-    let statusCode = 200;
-    let sucessMesssage = 'Success';
-    let number1 = req.query.number1;
-    let number2 = req.query.number2;
-    let result = parseInt(number1) + parseInt(number2);
+app.get('/api/cats', (req,res)=>{
+    getAllCats((err, result)=>{
+        if(!err){
+            res.json({statusCode:200, data:result, message:'Get allcats successful'})
+        }
+    });
+})
 
-    res.json({
-        message:sucessMesssage, 
-        code: statusCode, 
-        data: result
+app.post('/api/cat', (req, res)=>{
+    let cat = req.body;
+    postCat(cat, (err, result)=>{
+        if(!err){
+            res.json({statusCode:201, data:result, message:'success'})
+        }
+
     });
 });
 
-app.listen(port, () => {
-    console.log('server started on port 3000');
+function postCat(cat, callback){
+    collection.insertOne(cat, callback);
+}
+
+function getAllCats(callback){
+    collection.find({}).toArray(callback);
+}
+
+app.listen(port, ()=>{
+    console.log('express server started');
+    runDBConnection();
 });
